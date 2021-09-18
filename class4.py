19434fa418f7a2475b19580577d849236639b7ff
fangzhiyang <fanmcgradfy@gmail.com>
2021-09-18 15:47:10 +0800

    第四课代码

--- class4.py
+++ class4.py
@@ -1,3 +1,42 @@
+# 匿名函数
+# lambda_fun = lambda x, y, z : x * y * z
+#
+# print(type(lambda_fun))
+# print(lambda_fun(2, 3, 4))
+#
+# # filter
+# ll = [1, 2, 3, 4]
+# def filter_fun(i):
+# if i > 3:
+#     return i
+# else:
+#     return None
+# return i if i > 3 else None
 
-def foo():
-    print("hello")
+# print(list(filter(filter_fun, ll)))
+
+# 另一种等价的filter方法
+# ll2 = [i for i in ll if i > 3]
+# print(ll2)
+
+# map
+# list3 = [1, 2, 3, 4]
+
+# def map_fun(i):
+#     return "{}.txt".format(i)
+
+# result = map(map_fun, list3)
+# print(list(result))
+
+
+# def fun(x, y):
+#     return x * y, x + y, x / y
+#
+# m, a, s = fun(1, 2)
+
+# 教务处新闻爬虫程序
+## 1、分析网页结构
+
+## 2、Python获取网页源代码
+
+## 3、处理数据，获取新闻列表
