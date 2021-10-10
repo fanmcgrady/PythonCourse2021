3ad4022a9538ac9b7662c6a1bd173e9210326710
fangzhiyang <fanmcgradfy@gmail.com>
2021-10-10 23:08:00 +0800

    第五课代码

---
+++ program/wechat_sports.py
@@ -0,0 +1,27 @@
+## utf-8
+import subprocess
+
+class altera_system_console:
+    def __init__(self):
+        sc_path = r'program/wechat.exe'
+        self.console = subprocess.Popen(sc_path, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    def read_output(self):
+        return self.console.stdout.read1(100).decode('utf-8')
+
+    def cmd(self, cmd_string):
+        self.console.stdin.write(bytes(cmd_string+'\n','utf-8'))
+        self.console.stdin.flush()
+
+def set_step(step, username, password):
+    c = altera_system_console()
+    print(c.read_output())
+    c.cmd(step)
+    print(c.read_output())
+    c.cmd(username)
+    print(c.read_output())
+    c.cmd(password)
+    print(c.read_output())
+
+if __name__ == '__main__':
+    set_step("想刷的步数", "用户名", "密码")
